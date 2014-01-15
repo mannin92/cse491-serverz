883baefa57b6ee0e2fc652ee54949bd5a20d7c31
Jennifer Manning <mannin92@arctic.cse.msu.edu>
2014-01-15 14:51:19 -0500

    Log changed, and HTTP message sending

--- server.py
+++ server.py
@@ -18,6 +18,14 @@ while True:
     # Establish connection with client.    
     c, (client_host, client_port) = s.accept()
     print 'Got connection from', client_host, client_port
-    c.send('Thank you for connecting')
-    c.send("good bye.")
+    c.send('HTTP/1.0 200 OK\r\n') 
+    
+    #alternative way 
+    # s.send('%s %s %s' % (response_proto, response_status, response_status_text))
+
+    #header details
+    c.send('Content-Type: text/html\r\n\r\n')
+
+    response_body = "<h1>Hello, world!</h1> This is mannin92's web server\r\n"
+    c.send(response_body)
     c.close()
