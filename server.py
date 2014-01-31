9b42f5414d577122c029e46c97a12e5f188344a3
Jennifer Manning <mannin92@arctic.cse.msu.edu>
2014-01-31 12:15:04 -0500

    hw3 forgot to commit

--- server.py
+++ server.py
@@ -2,6 +2,7 @@
 import random
 import socket
 import time
+import urlparse
 
 def main():
 
@@ -25,30 +26,86 @@ def main():
 def handle_connection(conn):
     
     #Get the request message and parse
-    request = conn.recv(1000).split('\n')[0];
+    fullrequest = conn.recv(1000)
+    request=fullrequest.split('\n')[0];
     method = request.split(' ')[0];
-    path = request.split(' ')[1];
+    dirtyPath = request.split(' ')[1];
+    parse = urlparse.urlparse(dirtyPath);
+    path=parse[2];
 
     #Send a response
     conn.send('HTTP/1.0 200 OK\r\n');
     conn.send('Content-type: text/html\r\n\r\n');
     response_body='';
     if method == 'POST':
-        response_body = '<h1>GHOST!</h1>Oops, meant \"Post!\"';
-    elif path == '/':
-        response_body = '<h1>Link to the past</h1>'+ \
-                      '<a href = /content>Content</a><br>' + \
-                      '<a href = /file>File</a><br>' + \
-                      '<a href = /image>Image</a>'
-    elif path == '/content':
-        response_body = '<h1>Khan-tent</h1>Khaannnnnnnnnnn';
-    elif path == '/file':
-        response_body = '<h1>Let\'s go a-filing</h1> :D';
-    elif path == '/image':
-        response_body = '<h1>Gif</h1>Insert cute animal';
-
-    conn.send(response_body);
+        if path == '/submit':
+            submit_connection(conn, fullrequest.split('\r\n')[-1]);
+    elif method =='GET':
+        if path == '/':
+            response_body = '<h1>Link to the past</h1>'+ \
+                          '<a href = /content>Content</a><br>' + \
+                          '<a href = /file>File</a><br>' + \
+                          '<a href = /image>Image</a><br>' + \
+                          '<a href = /getform>GET Form</a><br>' +\
+                          '<a href = /postform>POST Form</a>'
+            index_connection(conn, response_body);
+        elif path == '/content':
+            response_body = '<h1>Khan-tent</h1>Khaannnnnnnnnnn';
+            content_connection(conn, response_body);
+        elif path == '/file':
+            response_body = '<h1>Let\'s go a-filing</h1> :D';
+            file_connection(conn, response_body);
+        elif path == '/image':
+            response_body = '<h1>Gif</h1>Insert cute animal';
+            image_connection(conn, response_body);
+        elif path =='/getform':
+            response_body = "<form action='/submit' method='GET'>\n" + \
+                "First name: <input type='text' name='firstname'><br> " + \
+                "Last name: <input type='text' name='lastname'>" + \
+                "<p><input type='submit' value='Submit'>\n\n" + \
+                "</form>"
+            form_connection(conn, response_body);    
+        elif path =='/postform':
+            response_body = "<form action='/submit' method='POST'>\n" + \
+                "First name: <input type='text' name='firstname'><br> " + \
+                "Last name: <input type='text' name='lastname'>" + \
+                "<p><input type='submit' value='Submit'>\n\n" + \
+                "</form>"
+            form_connection(conn, response_body);    
+        elif path == '/submit':
+            submit_connection(conn, parse[4]);
+		
     conn.close();
+    
+#this counts as refactoring rightttttt?
+def index_connection(conn, input):
+	conn.send(input);
+	
+def content_connection(conn, input):
+	conn.send(input);
+	
+def file_connection(conn, input):
+	conn.send(input);
+	
+def image_connection(conn, input):
+	conn.send(input);
+    
+def form_connection(conn, input):
+	conn.send(input);
 
+def submit_connection(conn, input):
+
+#stole this from Cam.
+    params = input.split("&")
+    
+    firstname = params[0].split("=")[1];
+    lastname = params[1].split("=")[1];
+
+    conn.send('HTTP/1.0 200 OK\r\n' + \
+              'Content-type: text/html\r\n' + \
+              '\r\n' + \
+              "Hello Mr. %s %s." % (firstname, lastname))
+    
+    
 if __name__ == '__main__':
     main();
