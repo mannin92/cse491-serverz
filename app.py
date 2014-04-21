094d0a3555746e768e60e50a6b92c6d5311efe25
Jennifer Manning <mannin92@arctic.cse.msu.edu>
2014-04-21 17:50:37 -0400

    all the things

--- app.py
+++ app.py
@@ -1,19 +1,14 @@
-#! /usr/bin/env python
+# from http://docs.python.org/2/library/wsgiref.html
 
 import cgi
+import urlparse
 import jinja2
-import os
-import traceback
-import urllib
+from wsgiref.util import setup_testing_defaults
 from StringIO import StringIO
-from urlparse import urlparse, parse_qs
-from wsgiref.simple_server import make_server
 
-def simple_app(environ, start_response):
+def app(environ, start_response):
     loader = jinja2.FileSystemLoader('./templates')
     env = jinja2.Environment(loader=loader)
-
-    print(environ)
 
     # By default, set up the 404 page response. If it's
     # a valid page, we change this. If some weird stuff
@@ -30,6 +25,10 @@ def simple_app(environ, start_response):
 
     if http_method == 'POST':
         if path == '/':
+            # I feel like there's a better way of doing this
+            # than spamming status = '200 OK'. But it's almost 10
+            # and we have to catch up because our capstone group
+            # member just didn't do anything the past week. /rant
             status = '200 OK'
             response_content = handle_index(environ, env)
         elif path == '/submit':
@@ -43,9 +42,11 @@ def simple_app(environ, start_response):
             status = '200 OK'
             response_content = handle_content(environ, env)
         elif path == '/file':
+            headers = [('Content-type', 'text/plain')]
             status = '200 OK'
             response_content = handle_file(environ, env)
         elif path == '/image':
+            headers = [('Content-type', 'image/jpeg')]
             status = '200 OK'
             response_content = handle_image(environ, env)
         elif path == '/submit':
@@ -53,53 +54,78 @@ def simple_app(environ, start_response):
             response_content = handle_submit_get(environ, env)
                 
     start_response(status, headers)
-    return response_content
+    response = []
+    response.append(response_content)
+    return response
 
 def make_app():
-    return simple_app
+    return app
 
 def handle_index(params, env):
-    return str(env.get_template("index.html").render())
+    return str(env.get_template("index_result.html").render())
     
 def handle_content(params, env):
-    return str(env.get_template("content.html").render())
+    return str(env.get_template("content_result.html").render())
+
+def readFile(filepath):
+    ''' Reads a file and returns its contents as a string '''
+    f = open(filepath, 'rb')
+    data = f.read()
+    f.close()
+
+    return data
 
 def handle_file(params, env):
-    return str(env.get_template("file.html").render())
+    return readFile('./files/butts.txt')
 
 def handle_image(params, env):
-    return str(env.get_template("image.html").render())
+    return readFile('./images/doge.jpeg')
 
 def not_found(params, env):
-    return str(env.get_template("404.html").render())
+    return str(env.get_template("not_found.html").render())
 
 def handle_submit_post(environ, env):
     ''' Handle a connection given path /submit '''
-    # submit needs to know about the query field, so more
-    # work needs to be done here.
-
-    # we want the first element of the returned list
+    
     headers = {}
     for k in environ.keys():
         headers[k.lower()] = environ[k]
+
+    headers['content-type'] = environ['CONTENT_TYPE']
+    headers['content-length'] = environ['CONTENT_LENGTH']    
+
+    firstNameFieldName = 'firstnamePOST2'
+    lastNameFieldName = 'lastnamePOST2'
+
+    # Kludgey workaround
+    if "multipart/form-data" in environ['CONTENT_TYPE']:
+        cLen = int(environ['CONTENT_LENGTH'])
+        data = environ['wsgi.input'].read(cLen)
+        environ['wsgi.input'] = StringIO(data)
+
+        firstNameFieldName = 'firstnamePOST1'
+        lastNameFieldName = 'lastnamePOST1'
 
     form = cgi.FieldStorage(headers = headers, fp = environ['wsgi.input'], 
                             environ = environ)
 
     try:
-      firstname = form['firstname'].value
+      firstname = form[firstNameFieldName].value
     except KeyError:
       firstname = ''
-    try:
-      lastname = form['lastname'].value
-    except KeyError:
-      lastname = ''
 
-    print form
+    try:
+      lastname = form[lastNameFieldName].value
+    except KeyError:
+        lastname = ''
+
     vars = dict(firstname = firstname, lastname = lastname)
-    return str(env.get_template("submit.html").render(vars))
+    return str(env.get_template("submit_result.html").render(vars))
 
 def handle_submit_get(environ, env):
+    ''' Handle a connection given path /submit '''
+    # submit needs to know about the query field, so more
+    # work needs to be done here.
 
     # we want the first element of the returned list
     params = environ['QUERY_STRING']
@@ -115,4 +141,4 @@ def handle_submit_get(environ, env):
       lastname = ''
 
     vars = dict(firstname = firstname, lastname = lastname)
-    return str(env.get_template("submit.html").render(vars))
+    return str(env.get_template("submit_result.html").render(vars))
--- imageapp/root.py
+++ imageapp/root.py
@@ -1,5 +1,7 @@
 import quixote
 from quixote.directory import Directory, export, subdir
+from quixote.util import StaticFile
+import os.path
 
 from . import html, image
 
@@ -10,9 +12,19 @@ class RootDirectory(Directory):
     def index(self):
         return html.render('index.html')
 
+    @export(name='jquery')
+    def jquery(self):
+        return open('jquery-1.11.0.min.js').read()
+
     @export(name='upload')
     def upload(self):
         return html.render('upload.html')
+
+    @export(name='set')
+    def set(self):
+        response = quixote.get_response()
+
+        print response
 
     @export(name='upload_receive')
     def upload_receive(self):
@@ -22,12 +34,11 @@ class RootDirectory(Directory):
         the_file = request.form['file']
         print dir(the_file)
         print 'received file with name:', the_file.base_filename
-        data = the_file.read(int(1e9))
+        data = the_file.read(the_file.get_size())
 
-        image.add_image(data)
+        image.add_image(the_file.base_filename, data)
 
         return quixote.redirect('./')
-
 
     @export(name='upload2')
     def upload2(self):
@@ -41,20 +52,121 @@ class RootDirectory(Directory):
         the_file = request.form['file']
         print dir(the_file)
         print 'received file with name:', the_file.base_filename
-        data = the_file.read(int(1e9))
+        data = the_file.read(the_file.get_size())
 
-        image.add_image(data)
+        image.add_image(the_file.base_filename, data)
 
         return html.render('upload2_received.html')
-
 
     @export(name='image')
     def image(self):
         return html.render('image.html')
 
+    @export(name='image_list')
+    def image_list(self):
+        return html.render('image_list.html')
+
+    @export(name='image_count')
+    def image_count(self):
+        return image.get_num_images()
+
     @export(name='image_raw')
     def image_raw(self):
         response = quixote.get_response()
-        response.set_content_type('image/png')
-        img = image.get_latest_image()
-        return img
+        request = quixote.get_request()
+
+        try:
+            i = int(request.form['num'])
+        except:
+            i = -1
+
+        img = image.retrieve_image(i)
+
+        filename = img.filename
+        if filename.lower() in ('jpg', 'jpeg'):
+            response.set_content_type('image/jpeg')
+        elif filename.lower() in ('tif',' tiff'):
+            response.set_content_type('image/tiff')
+        else: # Default to .png for reasons
+            response.set_content_type('image/png')
+        return img.data
+
+    @export(name='get_comments')
+    def get_comments(self):
+        response = quixote.get_response()
+        request = quixote.get_request()
+
+        try:
+            i = int(request.form['num'])
+        except:
+            i = -1
+
+        all_comments = []
+        for comment in image.get_comments(i):
+            all_comments.append("""\
+    <comment>
+     <text>%s</text>
+    </comment>
+    """ % (comment))
+
+        xml = """
+    <?xml version="1.0"?>
+    <comments>
+    %s
+    </comments>
+    """ % ("".join(all_comments))
+
+        return xml
+
+    @export(name='add_comment')
+    def add_comment(self):
+        response = quixote.get_response()
+        request = quixote.get_request()
+
+        try:
+            i = int(request.form['num'])
+        except:
+            i = -1
+
+        try:
+            comment = request.form['comment']
+        except:
+            return
+
+        image.add_comment(i, comment)
+
+    @export(name='get_score')
+    def get_score(self):
+        response = quixote.get_response()
+        request = quixote.get_request()
+
+        try:
+            i = int(request.form['num'])
+        except:
+            i = -1
+
+        return image.get_image_score(i)
+
+    @export(name='increment_score')
+    def increment_score(self):
+        response = quixote.get_response()
+        request = quixote.get_request()
+
+        try:
+            i = int(request.form['num'])
+        except:
+            i = -1
+
+        image.increment_image_score(i)
+
+    @export(name='decrement_score')
+    def decrement_score(self):
+        response = quixote.get_response()
+        request = quixote.get_request()
+
+        try:
+            i = int(request.form['num'])
+        except:
+            i = -1
+
+        image.decrement_image_score(i)
